a5a5fa89caa8cb9be8000edd4816115e087ac822
Danushri Prakash <danushri.saranyaprakash@gmail.com>
2026-01-07 13:14:11 +0530

    Add total patients counter to queue layout

--- mediqueue/lib/main.dart
+++ mediqueue/lib/main.dart
@@ -1,26 +1,3 @@
-// // // // import 'package:flutter/material.dart';
-// // // // import 'screens/welcome_screen.dart';
-
-// // // // void main() {
-// // // //   runApp(const MediQueueApp());
-// // // // }
-
-// // // // class MediQueueApp extends StatelessWidget {
-// // // //   const MediQueueApp({super.key});
-
-// // // //   @override
-// // // //   Widget build(BuildContext context) {
-// // // //     return MaterialApp(
-// // // //       title: 'MediQueue',
-// // // //       debugShowCheckedModeBanner: false,
-// // // //       theme: ThemeData(
-// // // //         primarySwatch: Colors.blue,
-// // // //         useMaterial3: true,
-// // // //       ),
-// // // //       home: const WelcomeScreen(),
-// // // //     );
-// // // //   }
-// // // // }
 
 
 
